a30c122f1abf11963cacedad5d07a922c848360c
Ins <2518949@gmail.com>
2025-12-01 13:58:20 +0500

    feat(app): integrate scroll progress bar and debug block

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,23 @@
 import { restaurants } from '../../constants/mock';
-import { Reviews } from '../reviews/reviews';
-import { RestaurantListItem } from '../restaurant/restaurant';
 import { Layout } from '../layout/layout';
 import { RestaurantTabs } from '../restaurant-tabs/restaurant-tabs';
+import { ReviewForm } from '../review-form/review-form';
+import { useScrollProgress } from '../scroll-progress/useScrollProgress';
+import { ProgressBar } from '../scroll-progress/progressBar';
+import { DebugScrollBlock } from '../scroll-progress/debugScrollBlock';
 
-export const App = ({ title }) => {
+export const App = ({ title, debugScroll = false }) => {
+  const progress = useScrollProgress();
+
   return (
     <Layout sidebar={<div>SIDEBAR</div>}>
-      <div className="some-class" style={{ color: 'red' }}>
+      <ProgressBar progress={progress} />
+
+      <div className="some-class" style={{ color: 'blue' }}>
         <h1>{title}</h1>
         <RestaurantTabs restaurants={restaurants} />
+        <ReviewForm />
+        {debugScroll && <DebugScrollBlock height={3000} />}
       </div>
     </Layout>
   );
