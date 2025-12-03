a0c23168945ada09b2d517ce712404ba2e23af39
Ins <2518949@gmail.com>
2025-12-03 23:40:08 +0500

    refactor: move scroll logic into ScrollProgressContainer component, keep only connection

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,21 +2,18 @@ import { restaurants } from '../../constants/mock';
 import { Layout } from '../layout/layout';
 import { RestaurantTabs } from '../restaurant-tabs/restaurant-tabs';
 import { ReviewForm } from '../review-form/review-form';
-import { useScrollProgress } from '../scroll-progress/useScrollProgress';
-import { ProgressBar } from '../scroll-progress/progressBar';
 import { DebugScrollBlock } from '../scroll-progress/debugScrollBlock';
+import { ScrollProgressContainer } from '../scroll-progress/scrollProgressContainer';
 
 export const App = ({ title, debugScroll = false }) => {
-  const progress = useScrollProgress();
-
   return (
     <Layout sidebar={<div>SIDEBAR</div>}>
-      <ProgressBar progress={progress} />
+      <ScrollProgressContainer />
 
       <div className="some-class" style={{ color: 'blue' }}>
         <h1>{title}</h1>
         <RestaurantTabs restaurants={restaurants} />
-        <ReviewForm />
+
         {debugScroll && <DebugScrollBlock height={3000} />}
       </div>
     </Layout>
