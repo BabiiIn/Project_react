f6a21c2872f8ed5bc031a8ec25b0df7ccbb41f0e
Ins <2518949@gmail.com>
2025-12-06 18:11:59 +0500

    refactor: clean up markup by removing redundant <li> elements

--- src/components/menu/dish.jsx
+++ src/components/menu/dish.jsx
@@ -3,7 +3,7 @@ import styles from './dish.module.css';
 
 export const Dish = ({ dish }) => {
   return (
-    <li className={styles.dish}>
+    <div className={styles.dish}>
       <div className={styles.info}>
         <span className={styles.name}>{dish.name}</span>
         <span className={styles.price}>{dish.price} ₽</span>
@@ -11,6 +11,6 @@ export const Dish = ({ dish }) => {
       <div className={styles.counter}>
         <DishCounter _dishId={dish.id} />
       </div>
-    </li>
+    </div>
   );
 };
