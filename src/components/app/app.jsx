9caeb731ada138e2c106ec2a3f6572cd0244c772
Ins <2518949@gmail.com>
2025-12-07 14:13:15 +0500

    feat(app): wrap application with ThemeProvider and UserProvider

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,12 +2,20 @@ import { restaurants } from '../../constants/mock';
 import { Layout } from '../layout/layout';
 import { RestaurantsPage } from '../pages/restaurants-page';
 import styles from './app.module.css';
+import { ThemeProvider } from '../../context/theme-context';
+import { Header } from '../header/header';
+import { UserProvider } from '../../context/user-context';
 
 export const App = ({ title, debugScroll = false }) => {
   return (
-    <Layout debugScroll={debugScroll}>
-      <h1 className={styles.appTitle}>{title}</h1>
-      <RestaurantsPage restaurants={restaurants} />
-    </Layout>
+    <ThemeProvider>
+      <UserProvider>
+        <Layout debugScroll={debugScroll}>
+          <Header />
+          <h1 className={styles.appTitle}>{title}</h1>
+          <RestaurantsPage restaurants={restaurants} />
+        </Layout>
+      </UserProvider>
+    </ThemeProvider>
   );
 };
