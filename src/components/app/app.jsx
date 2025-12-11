4f0a80a0ac7b6492596c5185643d4931f6cc66a8
Ins <2518949@gmail.com>
2025-12-11 15:01:20 +0500

    feat: wrap App component with Redux Provider and store

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,27 @@
-import { restaurants } from '../../constants/mock';
+// import { restaurants } from '../../constants/mock';
 import { Layout } from '../layout/layout';
 import { RestaurantsPage } from '../pages/restaurants-page';
 import styles from './app.module.css';
 import { ThemeProvider } from '../../context/theme-context';
 import { Header } from '../header/header';
 import { UserProvider } from '../../context/user-context';
+import { Provider } from 'react-redux';
+import { store } from '../../redux/store';
 
 export const App = ({ title, debugScroll = false }) => {
   return (
-    <ThemeProvider>
-      <UserProvider>
-        <Layout debugScroll={debugScroll}>
-          <Header />
-          <h1 className={styles.appTitle}>{title}</h1>
-          <RestaurantsPage restaurants={restaurants} />
-        </Layout>
-      </UserProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <UserProvider>
+          <Layout debugScroll={debugScroll}>
+            <Header />
+            <h1 className={styles.appTitle}>{title}</h1>
+            {/* 👇 убираем restaurants пропс */}
+            <RestaurantsPage />
+            {/* <RestaurantsPage restaurants={restaurants} /> */}
+          </Layout>
+        </UserProvider>
+      </ThemeProvider>
+    </Provider>
   );
 };
