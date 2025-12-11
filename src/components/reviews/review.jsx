c940bda5047fa72d344a0d4185432bacb93bb115
Ins <2518949@gmail.com>
2025-12-11 15:11:56 +0500

    feat: fetch user data from Redux store in Review component

--- src/components/reviews/review.jsx
+++ src/components/reviews/review.jsx
@@ -1,10 +1,16 @@
+import { useSelector } from 'react-redux';
 import styles from './review.module.css';
 
+// export const Review = ({ review }) => {
+
 export const Review = ({ review }) => {
+  // достаём пользователя по userId
+  const user = useSelector((state) => state.users[review.userId]);
+
   return (
     <li className={styles.review}>
       <div className={styles.header}>
-        <span className={styles.author}>{review.user}</span>
+        <span className={styles.author}>{user ? user.name : 'Аноним'}</span>
         <span className={styles.rating}>Рейтинг: {review.rating}</span>
       </div>
       <p className={styles.text}>{review.text}</p>
