373859065b9c4648497deddc24498c5bf9b80829
Ins <2518949@gmail.com>
2025-12-11 15:07:20 +0500

    feat: fetch restaurants from Redux store in RestaurantsPage component

--- src/components/pages/restaurants-page.jsx
+++ src/components/pages/restaurants-page.jsx
@@ -1,24 +1,30 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux'; // 👈 добавляем
 import { Restaurant } from '../restaurants/restaurant';
 import { Tab } from '../tab/tab';
 import styles from './restaurants-page.module.css';
 
+export const RestaurantsPage = () => {
+  // достаём рестораны из Redux
+  const restaurants = useSelector((state) => state.restaurants);
 
-export const RestaurantsPage = ({ restaurants = [] }) => {
+  // превращаем объект в массив
+  const restaurantsArray = Object.values(restaurants);
+
   const [activeId, setActiveId] = useState(
-    restaurants.length > 0 ? restaurants[0].id : null
+    restaurantsArray.length > 0 ? restaurantsArray[0].id : null
   );
 
   if (restaurants.length === 0) {
     return <p>Рестораны пока не загружены.</p>;
   }
 
-  const activeRestaurant = restaurants.find((r) => r.id === activeId);
+  const activeRestaurant = restaurantsArray.find((r) => r.id === activeId);
 
   return (
     <div>
       <div className={styles.tabsContainer}>
-        {restaurants.map((restaurant) => (
+        {restaurantsArray.map((restaurant) => (
           <Tab
             key={restaurant.id}
             label={restaurant.name}
